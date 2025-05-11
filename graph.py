70daee072a1b93b8ac242964d27799459baa8c65
Nikhitamanjunath <63408492+Nikhitamanjunath@users.noreply.github.com>
2025-05-11 13:51:52 -0700

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 
-# Function to read the file and extract iteration and pcost values
 def extract_values(filename):
     iterations = []
     pcost = []
@@ -12,11 +11,9 @@ def extract_values(filename):
             columns = line.split()
             if len(columns) > 2:
                 try:
-                    # Extract iteration number from the first column and pcost from the second column
                     iterations.append(int(columns[0]))
                     pcost.append(float(columns[1]))
                 except ValueError:
-                    # Skip lines that do not contain valid integer or float values
                     continue
 
     return iterations, pcost
@@ -33,13 +30,10 @@ def plot_iteration_vs_pcost(iterations, pcost):
     plt.show()
 
 
-# Main execution
 filename = '/Users/pradeeppatil/workspace/ECE647_Project/output.txt'  # Change this to the actual path of your output file
 
-# Extract iterations and pcost values
 iterations, pcost = extract_values(filename)
 
 print(iterations, pcost)
 
-# Plot the graph
 plot_iteration_vs_pcost(iterations, pcost)
